74e6bdf505e1cafddfbc252b8c1c537520b03fd5
sebaspasker <sebas666chulo@gmail.com>
2020-03-19 20:21:15 +0100

    x

--- .config/i3/bar.sh
+++ .config/i3/bar.sh
@@ -5,6 +5,8 @@ MESSAGE2="CARGAR DISPOSITIVO / $(cat /sys/class/power_supply/BAT1/capacity)% 
 
 while :
 do
+	MESSAGE1="/ $(cat /sys/class/power_supply/BAT1/capacity)% | / $(date)"
+  MESSAGE2="CARGAR DISPOSITIVO / $(cat /sys/class/power_supply/BAT1/capacity)% | / $(date)"
 	STATE=$(cat /sys/class/power_supply/BAT1/status )
 	CAPACITY=$(cat /sys/class/power_supply/BAT1/capacity)
 	if [[ $STATE == 'Discharging' && (( $CAPACITY < 15 ))]]; then
