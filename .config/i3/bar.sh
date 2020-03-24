aa8d17339b607286c7be452521f0160481786ac1
sebaspasker <sebas666chulo@gmail.com>
2020-03-25 00:00:13 +0100

    Modified config file i3 and vimrc

--- .config/i3/bar.sh
+++ .config/i3/bar.sh
@@ -2,9 +2,11 @@
 
 while :
 do
-	VOLUME="$(amixer get 'Master' | grep % | cut -d" " -f6 | cut -c2-4)"
-	MESSAGE1="/ $(cat /sys/class/power_supply/BAT1/capacity)% | $VOLUME | / $(light) | / $(date)"
-  MESSAGE2="CARGAR DISPOSITIVO / $(cat /sys/class/power_supply/BAT1/capacity)% | $VOLUME | / $(light) | / $(date)"
+	VOLUME="$(amixer get 'Master' | grep % | cut -d" " -f6 | cut -c2-3)"
+	LIGHT="$(light | cut -c1-2)"
+	BATTERY="$(cat /sys/class/power_supply/BAT1/capacity)"
+	MESSAGE1="/ $BATTERY % | / $VOLUME % | / $LIGHT % | / $(date)"
+  MESSAGE2="CARGAR DISPOSITIVO / $BATTERY % | / $VOLUME % | / $LIGHT % | / $(date)"
 	STATE=$(cat /sys/class/power_supply/BAT1/status )
 	CAPACITY=$(cat /sys/class/power_supply/BAT1/capacity)
 	if [[ $STATE == 'Discharging' && (( $CAPACITY < 15 ))]]; then
